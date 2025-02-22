2c7d7c0dec7282f29218ff9c57d104e3541d6c57
sabihDev <8203sabi.rahman@gmail.com>
2025-02-22 20:46:15 +0500

    add password input component and update login page with new styles

--- frontend/travel-app/src/pages/Auth/Login.jsx
+++ frontend/travel-app/src/pages/Auth/Login.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
+import PasswordInput from "../../components/Input/PasswordInput";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [passsord, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default Login
+  const navigate = useNavigate();
+
+  const handleLogin = () => {};
+  return (
+    <div className="h-screen bg-cyan-100 overflow-hidden relative">
+      <div className="login-ui-box right-10 -top-40" />
+      <div className="login-ui-box bg-cyan-200 right-1/2 -bottom-40" />
+
+      <div className="container h-screen flex items-center justify-center mx-auto">
+        <div className="w-2.5/4 h-[80vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50">
+          <div>
+            <h4 className="text-5xl text-white font-semibold leading-[58px]">
+              Every Journey <br /> Tells A Story
+            </h4>
+            <p className="text-[15px] text-white leading-6 pr-7 mt-4">
+              Transform your adventures into timeless memories. <br /> Share
+              your world, one destination at a time.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-1.5/4 h-[70vh] bg-white p-16 rounded-r-lg shadow-lg z-10 shadow-cyan-200/20">
+          <form onSubmit={handleLogin}>
+            <h4 className="text-2xl font-semibold mb-7">Login</h4>
+
+            <input
+              type="email"
+              value={email}
+              onChange={(target) => setEmail(target.value)}
+              placeholder="Email"
+              className="input-box"
+            />
+
+            <PasswordInput
+              value={passsord}
+              onChange={(target) => setPassword(target.value)}
+            />
+
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+
+            <p className="text-xs text-slate-500 text-center my-4">Or</p>
+
+            <button
+              type="submit"
+              className="btn-primary btn-light"
+              onClick={() => {
+                navigate("/signup");
+              }}
+            >
+              CREATE ACCOUNT
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/travel-app/tailwind.config.js
+++ frontend/travel-app/tailwind.config.js
@@ -11,10 +11,13 @@ export default {
       display:["Poppins","sans-serif"]
     },
     extend: {
-       colors: {
+      colors: {
         primary: '#05B6D3',
         secondary: '#EF863E',
       },
+      backgroundImage:{
+        "login-bg-img":"url('./src/assets/images/forest.jpg')"
+      }
     },
   },
   plugins: [],
